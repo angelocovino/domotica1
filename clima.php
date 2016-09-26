34424c2267b1fda3ddb04f653d9d23ac05617f9c
angelocovino <angelo_covino@hotmail.com>
2016-09-26 15:57:38 +0200

    clima finito quasi

--- clima.php
+++ clima.php
@@ -15,26 +15,42 @@
         new climate("freddo", "93")
     );
 ?>
-<div class='centeredContainer'>
+<table class='centeredContainer'>
+    <tr>
+        <td>
 <?php
     foreach($climi as $clima){
         echo "<div class='bigDiv' id='{$clima->name}'>";
-            echo "<h1>{$clima->name}</h1>";
+            echo "<h1>{$clima->name}, <span class='stato'>Spento</span></h1>";
             echo "<div class='bigDivChild'>";
-                echo "<select class='sogliaSelect'>";
-                    for($i=12; $i<37; $i+=0.5){
-                        echo "<option value=\"{$i}\">{$i}</option>";
-                    }
-                echo "</select>";
-                echo "<button onClick=\"climateManagement('update',{$clima->port})\">Cambia soglia</button><br />";
-                echo "Stato <span class='stato'></span><br />";
-                echo "Temperatura <span class='temperature'></span><br />";
-                echo "Soglia<span class='soglia'></span><br />";
-                echo "Automatico<div onClick=\"climateManagement('automatic',{$clima->port})\"><img src='' class='automatico' /></div>";
-                echo "Manuale<div onClick=\"climateManagement('manual',{$clima->port})\"><img src='' class='manuale' /></div>";
+                echo "<ul>";
+                echo "<li>";
+                    echo "Temperatura: <span class='temperature'>-</span><br />";
+                echo "</li>";
+                echo "<li>";
+                    echo "Soglia: <span class='soglia'></span><br />";
+                    echo "<div class='comparoScomparo'>";
+                        echo "Cambia: <select class='sogliaSelect'>";
+                            for($i=12; $i<37; $i+=0.5){
+                                echo "<option value=\"{$i}\">{$i}</option>";
+                            }
+                        echo "</select>";
+                        echo "<button onClick=\"climateManagement('update',{$clima->port})\">Salva</button><br />";
+                    echo "</div>";
+                echo "</li>";
+                echo "<li>";
+                    echo "<span class='automaticManual'>Automatico/Manuale</span><br />";
+                    echo "<div class='comparoScomparo'>";
+                        echo "<div class='automatico' onClick=\"climateManagement('automatic',{$clima->port})\"><img src='immagini/switchOff.svg' class='automatico' /> Automatico</div>";
+                        echo "<div class='manuale' onClick=\"climateManagement('manual',{$clima->port})\"><img src='immagini/switchOff.svg' class='manuale' /> Manuale</div>";
+                    echo "</div>";
+                echo "</li>";
+                echo "</ul>";
             echo "</div>";
         echo "</div>";
     }
 ?>
-</div>
+        </td>
+    </tr>
+</table>
 <?php @include('shared/footer.php'); ?>
